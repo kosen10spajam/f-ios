4d8352685c5306e44f36567634a12c3df3374a30
denari01 <denari.c@icloud.com>
2016-04-03 02:16:28 +0900

    Initial Commit

---
+++ f-iosTests/f_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  f_iosTests.swift
+//  f-iosTests
+//
+//  Created by DeNAri on 2016/04/03.
+//  Copyright © 2016年 denari01. All rights reserved.
+//
+
+import XCTest
+@testable import f_ios
+
+class f_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
