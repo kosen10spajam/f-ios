0c798a048ad761c70c113ef8362fe2b5ee31b7d0
denari01 <denari.c@icloud.com>
2016-04-10 08:44:25 +0900

    custom for visual

--- f-ios/ViewController.swift
+++ f-ios/ViewController.swift
@@ -11,12 +11,20 @@ import UIKit
 class ViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var navigationBarItem: UINavigationItem!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var loadingView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let titleImageView = UIImageView(image: UIImage(named: "logo"))
+        self.navigationBarItem.titleView = titleImageView;
         self.webView.delegate = self;
-        let url = NSURL(string: "http://google.com/")
+        let url = NSURL(string: "https://sonoba-webclient.herokuapp.com/#/")
         let urlRequest = NSURLRequest(URL: url!)
         self.webView.loadRequest(urlRequest)
+        self.animationStart()
+        self.webView.opaque = true
+        self.webView.backgroundColor = UIColor.clearColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +36,39 @@ class ViewController: UIViewController, UIWebViewDelegate {
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         return true
     }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        self.animationEnd()
+    }
+    
+    //MARK: - Animation
+
+    func animationStart() {
+        let animateOption:UIViewAnimationOptions = [.Repeat, .CurveEaseInOut, .Autoreverse]
+        UIView.animateWithDuration(
+            1.0,
+            delay: 0.0,
+            options: animateOption,
+            animations: {() -> Void in
+                self.logoImageView.transform = CGAffineTransformMakeScale(0.8, 0.8)
+            },
+            completion: nil)
+    }
+    
+    func animationEnd() {
+        let animateOption:UIViewAnimationOptions = [.CurveEaseInOut]
+        UIView.animateWithDuration(
+            1.0,
+            delay: 0.0,
+            options: animateOption,
+            animations: {() -> Void in
+                self.loadingView.alpha = 0
+            },
+            completion: nil)
+        
+    }
+    
+    //MARK: - Browser
 
     @IBAction func refreshButtonTapped(sender: AnyObject) {
         self.webView.reload()
@@ -36,5 +77,10 @@ class ViewController: UIViewController, UIWebViewDelegate {
     @IBAction func backButtonTapped(sender: AnyObject) {
         self.webView.goBack()
     }
+    
+    @IBAction func closeKeyBoard(sender: AnyObject) {
+        self.webView.endEditing(true)
+    }
+    
 }
 
